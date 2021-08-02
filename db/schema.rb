b293c998b9a9f128483214c0a72e9f6ca444a89b
Kris Hopper <hopperdavis@gmail.com>
2021-08-02 13:17:53 -0700

    WIP: rails project setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_08_02_173036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "genre"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.string "lyrics"
+    t.integer "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "songs", "albums"
 end
